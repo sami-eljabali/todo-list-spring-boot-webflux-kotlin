27507c41318553433d4bfbca901809c6162ee6c9
Sami Eljabali <sami@eljabali.org>
2026-05-09 15:33:43 -0700

    Revert "tests"
    This reverts commit 99c95d870ac779d8f66502ff9c7ad01575cc95ea.

---
+++ src/test/resources/init.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE IF NOT EXISTS todos_id_seq;
+
+CREATE TABLE IF NOT EXISTS todos(
+id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('todos_id_seq') ,
+title VARCHAR(255) NOT NULL,
+status VARCHAR(255) NOT NULL,
+created_by VARCHAR(255),
+created_at TIMESTAMP
+);
+
+ALTER SEQUENCE todos_id_seq OWNED BY todos.id;
